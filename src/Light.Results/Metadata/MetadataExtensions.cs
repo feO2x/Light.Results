514a26c6dc7e3dfe98ddb64bff374fe3dc2c9a33
Kenny Pflug <kenny.pflug@live.de>
2026-01-14 08:21:28 +0100

    chore: fix NRT warnings
    This is actually related to C# 14 Extension Blocks. I had to revert back to Extension Methods to fix this. See MetadataExtensions.cs for details.
    
    Signed-off-by: Kenny Pflug <kenny.pflug@live.de>

--- src/Light.Results/Metadata/MetadataExtensions.cs
+++ src/Light.Results/Metadata/MetadataExtensions.cs
@@ -1,3 +1,18 @@
+// ReSharper disable ConvertToExtensionBlock -- as of 2026-01-14, the C# compiler produces a warning for
+// extension blocks in combination with Nullable Reference Types, which is why we resort back to extension methods.
+/* /Users/kenny/Code/Light.Results/src/Light.Results/MetadataExtensions/Tracing.cs(30,34): error CS8620: Argument of type '(string Key, MetadataValue Value)' cannot be used for parameter 'properties' of type '(string Key, MetadataValue Value)[]' in 'T extension<T>(T).MergeMetadata(params (string Key, MetadataValue Value)[] properties)' due to differences in the nullability of reference types. [/Users/kenny/Code/Light.Results/src/Light.Results/Light.Results.csproj]
+   /Users/kenny/Code/Light.Results/src/Light.Results/MetadataExtensions/Tracing.cs(38,34): error CS8620: Argument of type '(string Key, MetadataValue Value)' cannot be used for parameter 'properties' of type '(string Key, MetadataValue Value)[]' in 'T extension<T>(T).MergeMetadata(params (string Key, MetadataValue Value)[] properties)' due to differences in the nullability of reference types. [/Users/kenny/Code/Light.Results/src/Light.Results/Light.Results.csproj]
+   /Users/kenny/Code/Light.Results/src/Light.Results/MetadataExtensions/Tracing.cs(52,17): error CS8620: Argument of type '(string Key, MetadataValue Value)' cannot be used for parameter 'properties' of type '(string Key, MetadataValue Value)[]' in 'T extension<T>(T).MergeMetadata(params (string Key, MetadataValue Value)[] properties)' due to differences in the nullability of reference types. [/Users/kenny/Code/Light.Results/src/Light.Results/Light.Results.csproj]
+   /Users/kenny/Code/Light.Results/src/Light.Results/MetadataExtensions/Tracing.cs(53,17): error CS8620: Argument of type '(string Key, MetadataValue Value)' cannot be used for parameter 'properties' of type '(string Key, MetadataValue Value)[]' in 'T extension<T>(T).MergeMetadata(params (string Key, MetadataValue Value)[] properties)' due to differences in the nullability of reference types. [/Users/kenny/Code/Light.Results/src/Light.Results/Light.Results.csproj]
+       0 Warning(s)
+       4 Error(s)
+
+   Time Elapsed 00:00:00.47
+*/
+// This occurs when we use an extension block in this MetadataExtensions.cs file - I have no clue, why this is happening - need to report this as a bug to Microsoft.
+// .NET 10.0.100 SDK with C# 14.0
+
+
 namespace Light.Results.Metadata;
 
 /// <summary>
@@ -5,40 +20,44 @@ namespace Light.Results.Metadata;
 /// </summary>
 public static class MetadataExtensions
 {
+    /// <summary>
+    /// Returns a new instance with no metadata.
+    /// </summary>
     /// <param name="result">The instance to clear metadata from.</param>
     /// <typeparam name="T">The type implementing <see cref="IHasOptionalMetadata{T}" />.</typeparam>
-    extension<T>(T result) where T : struct, IHasOptionalMetadata<T>
+    /// <returns>A new instance with no metadata.</returns>
+    public static T ClearMetadata<T>(this T result) where T : struct, IHasOptionalMetadata<T> =>
+        result.ReplaceMetadata(null);
+
+    /// <summary>
+    /// Merges the specified metadata with the metadata of this instance and returns a new instance.
+    /// </summary>
+    /// <param name="properties">The metadata properties to merge.</param>
+    /// <param name="result">The instance to clear metadata from.</param>
+    /// <typeparam name="T">The type implementing <see cref="IHasOptionalMetadata{T}" />.</typeparam>
+    /// <returns>A new instance with the merged metadata.</returns>
+    public static T MergeMetadata<T>(this T result, params (string Key, MetadataValue Value)[] properties)
+        where T : struct, IHasOptionalMetadata<T>
     {
-        /// <summary>
-        /// Returns a new instance with no metadata.
-        /// </summary>
-        /// <returns>A new instance with no metadata.</returns>
-        public T ClearMetadata() => result.ReplaceMetadata(null);
+        var newMetadata = result.Metadata?.With(properties) ?? MetadataObject.Create(properties);
+        return result.ReplaceMetadata(newMetadata);
+    }
 
-        /// <summary>
-        /// Merges the specified metadata with the metadata of this instance and returns a new instance.
-        /// </summary>
-        /// <param name="properties">The metadata properties to merge.</param>
-        /// <returns>A new instance with the merged metadata.</returns>
-        public T MergeMetadata(params (string Key, MetadataValue Value)[] properties)
-        {
-            var newMetadata = result.Metadata?.With(properties) ?? MetadataObject.Create(properties);
-            return result.ReplaceMetadata(newMetadata);
-        }
-
-        /// <summary>
-        /// Merges the specified metadata with the metadata of this instance and returns a new instance.
-        /// </summary>
-        /// <param name="other">The metadata to merge.</param>
-        /// <param name="strategy">The merge strategy to use.</param>
-        /// <returns>A new instance with the merged metadata.</returns>
-        public T MergeMetadata(
-            MetadataObject other,
-            MetadataMergeStrategy strategy = MetadataMergeStrategy.AddOrReplace
-        )
-        {
-            var merged = MetadataObjectExtensions.MergeIfNeeded(result.Metadata, other, strategy);
-            return merged is null ? result : result.ReplaceMetadata(merged.Value);
-        }
+    /// <summary>
+    /// Merges the specified metadata with the metadata of this instance and returns a new instance.
+    /// </summary>
+    /// <param name="other">The metadata to merge.</param>
+    /// <param name="strategy">The merge strategy to use.</param>
+    /// <param name="result">The instance to clear metadata from.</param>
+    /// <typeparam name="T">The type implementing <see cref="IHasOptionalMetadata{T}" />.</typeparam>
+    /// <returns>A new instance with the merged metadata.</returns>
+    public static T MergeMetadata<T>(
+        this T result,
+        MetadataObject other,
+        MetadataMergeStrategy strategy = MetadataMergeStrategy.AddOrReplace
+    ) where T : struct, IHasOptionalMetadata<T>
+    {
+        var merged = MetadataObjectExtensions.MergeIfNeeded(result.Metadata, other, strategy);
+        return merged is null ? result : result.ReplaceMetadata(merged.Value);
     }
 }
--- src/Light.Results/MetadataExtensions/Tracing.cs
+++ src/Light.Results/MetadataExtensions/Tracing.cs
@@ -1,3 +1,6 @@
+// ReSharper disable ConvertToExtensionBlock -- see comment in MetadataExtensions.cs for details
+
+
 using Light.Results.Metadata;
 
 namespace Light.Results.MetadataExtensions;
@@ -17,68 +20,77 @@ public static class Tracing
     /// </summary>
     public const string CorrelationIdKey = "correlationId";
 
+    /// <summary>
+    /// Returns a new result with the specified source added to metadata.
+    /// </summary>
+    /// <param name="source">The source identifier (e.g., service name).</param>
     /// <param name="result">The result to add source metadata to.</param>
     /// <typeparam name="T">The type implementing <see cref="IHasOptionalMetadata{T}" />.</typeparam>
-    extension<T>(T result) where T : struct, IHasOptionalMetadata<T>
+    /// <returns>A new result with the source metadata.</returns>
+    public static T WithSource<T>(this T result, string source) where T : struct, IHasOptionalMetadata<T> =>
+        result.MergeMetadata((SourceKey, source));
+
+    /// <summary>
+    /// Returns a new result with the specified correlation ID added to metadata.
+    /// </summary>
+    /// <param name="correlationId">The correlation ID (e.g., trace ID, request ID).</param>
+    /// <param name="result">The result to add source metadata to.</param>
+    /// <typeparam name="T">The type implementing <see cref="IHasOptionalMetadata{T}" />.</typeparam>
+    /// <returns>A new result with the correlation ID metadata.</returns>
+    public static T WithCorrelationId<T>(this T result, string correlationId)
+        where T : struct, IHasOptionalMetadata<T> => result.MergeMetadata((CorrelationIdKey, correlationId));
+
+    /// <summary>
+    /// Returns a new result with the specified tracing metadata added.
+    /// </summary>
+    /// <param name="source">The source identifier (e.g., service name).</param>
+    /// <param name="correlationId">The correlation ID (e.g., trace ID, request ID).</param>
+    /// <param name="result">The result to add source metadata to.</param>
+    /// <typeparam name="T">The type implementing <see cref="IHasOptionalMetadata{T}" />.</typeparam>
+    /// <returns>A new result with the tracing metadata.</returns>
+    public static T WithTracing<T>(
+        this T result,
+        string source,
+        string correlationId
+    ) where T : struct, IHasOptionalMetadata<T>
     {
-        /// <summary>
-        /// Returns a new result with the specified source added to metadata.
-        /// </summary>
-        /// <param name="source">The source identifier (e.g., service name).</param>
-        /// <returns>A new result with the source metadata.</returns>
-        public T WithSource(string source) => result.MergeMetadata((SourceKey, source));
+        return result.MergeMetadata((SourceKey, source), (CorrelationIdKey, correlationId));
+    }
 
-        /// <summary>
-        /// Returns a new result with the specified correlation ID added to metadata.
-        /// </summary>
-        /// <param name="correlationId">The correlation ID (e.g., trace ID, request ID).</param>
-        /// <returns>A new result with the correlation ID metadata.</returns>
-        public T WithCorrelationId(string correlationId) => result.MergeMetadata((CorrelationIdKey, correlationId));
-
-        /// <summary>
-        /// Returns a new result with the specified tracing metadata added.
-        /// </summary>
-        /// <param name="source">The source identifier (e.g., service name).</param>
-        /// <param name="correlationId">The correlation ID (e.g., trace ID, request ID).</param>
-        /// <returns>A new result with the tracing metadata.</returns>
-        public T WithTracing(
-            string source,
-            string correlationId
-        )
+    /// <summary>
+    /// Attempts to retrieve the source identifier from the result's metadata.
+    /// </summary>
+    /// <param name="source">The source identifier if found; otherwise, <c>null</c>.</param>
+    /// <param name="result">The result to add source metadata to.</param>
+    /// <typeparam name="T">The type implementing <see cref="IHasOptionalMetadata{T}" />.</typeparam>
+    /// <returns><c>true</c> if the source was found; otherwise, <c>false</c>.</returns>
+    public static bool TryGetSource<T>(this T result, out string? source) where T : struct, IHasOptionalMetadata<T>
+    {
+        if (result.Metadata?.TryGetString(SourceKey, out source) == true)
         {
-            return result.MergeMetadata((SourceKey, source), (CorrelationIdKey, correlationId));
+            return true;
         }
 
-        /// <summary>
-        /// Attempts to retrieve the source identifier from the result's metadata.
-        /// </summary>
-        /// <param name="source">The source identifier if found; otherwise, <c>null</c>.</param>
-        /// <returns><c>true</c> if the source was found; otherwise, <c>false</c>.</returns>
-        public bool TryGetSource(out string? source)
-        {
-            if (result.Metadata?.TryGetString(SourceKey, out source) == true)
-            {
-                return true;
-            }
+        source = null;
+        return false;
+    }
 
-            source = null;
-            return false;
+    /// <summary>
+    /// Attempts to retrieve the correlation ID from the result's metadata.
+    /// </summary>
+    /// <param name="correlationId">The correlation ID if found; otherwise, <c>null</c>.</param>
+    /// <param name="result">The result to add source metadata to.</param>
+    /// <typeparam name="T">The type implementing <see cref="IHasOptionalMetadata{T}" />.</typeparam>
+    /// <returns><c>true</c> if the correlation ID was found; otherwise, <c>false</c>.</returns>
+    public static bool TryGetCorrelationId<T>(this T result, out string? correlationId)
+        where T : struct, IHasOptionalMetadata<T>
+    {
+        if (result.Metadata?.TryGetString(CorrelationIdKey, out correlationId) == true)
+        {
+            return true;
         }
 
-        /// <summary>
-        /// Attempts to retrieve the correlation ID from the result's metadata.
-        /// </summary>
-        /// <param name="correlationId">The correlation ID if found; otherwise, <c>null</c>.</param>
-        /// <returns><c>true</c> if the correlation ID was found; otherwise, <c>false</c>.</returns>
-        public bool TryGetCorrelationId(out string? correlationId)
-        {
-            if (result.Metadata?.TryGetString(CorrelationIdKey, out correlationId) == true)
-            {
-                return true;
-            }
-
-            correlationId = null;
-            return false;
-        }
+        correlationId = null;
+        return false;
     }
 }
