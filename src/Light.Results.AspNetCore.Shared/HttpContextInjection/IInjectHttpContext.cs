ac2ed07512b7c53741091fe06119603347727d93
Kenny Pflug <kenny.pflug@live.de>
2026-01-31 07:00:53 +0100

    chore: remove obsolete HttpContextInjectionMiddleware
    Signed-off-by: Kenny Pflug <kenny.pflug@live.de>

--- src/Light.Results.AspNetCore.Shared/HttpContextInjection/IInjectHttpContext.cs
+++
@@ -1,8 +0,0 @@
-using Microsoft.AspNetCore.Http;
-
-namespace Light.Results.AspNetCore.Shared.HttpContextInjection;
-
-public interface IInjectHttpContext
-{
-    HttpContext HttpContext { set; }
-}
