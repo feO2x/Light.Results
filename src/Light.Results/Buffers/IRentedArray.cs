08824c232ca41a185ed00b3bafe4779151109d98
Kenny Pflug <kenny.pflug@live.de>
2026-02-23 07:06:34 +0100

    chore: fix XML comments in IRentedArray
    Signed-off-by: Kenny Pflug <kenny.pflug@live.de>

--- src/Light.Results/Buffers/IRentedArray.cs
+++ src/Light.Results/Buffers/IRentedArray.cs
@@ -11,12 +11,12 @@ public interface IRentedArray : IDisposable
     /// <summary>
     /// Gets a ReadOnlyMemory&lt;byte> that provides read-only access to the pooled array data.
     /// </summary>
-    /// <exception cref="InvalidOperationException">Thrown when the PooledArray has been disposed.</exception>
+    /// <exception cref="InvalidOperationException">Thrown when the rented array has been disposed.</exception>
     ReadOnlyMemory<byte> Memory { get; }
 
     /// <summary>
     /// Gets a ReadOnlySpan&lt;byte> that provides read-only access to the pooled array data.
     /// </summary>
-    /// <returns>A ReadOnlySpan&lt;byte> wrapping the pooled array data.</returns>
+    /// <exception cref="InvalidOperationException">Thrown when the rented array has been disposed.</exception>
     ReadOnlySpan<byte> Span { get; }
 }
